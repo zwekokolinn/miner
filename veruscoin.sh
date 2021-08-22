8a99d5f52ceed28b379731e7a9882b19eaa9725d
zwekokolinn <85805922+zwekokolinn@users.noreply.github.com>
2021-08-22 14:14:44 +0630

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,5 +1,5 @@
 #veruscoin
 sudo apt update
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar
+tar xf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RVhLoP1oyzZafu5GbRoJXt5m7HG5LKEapM.zweko -p x --cpu 3
